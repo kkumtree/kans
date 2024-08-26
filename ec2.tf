861a728ccccf528ac4ed3bc60b5db6a4dd1b4242
kkumtree <mldd.kor@gmail.com>
2024-08-27 02:36:51 +0900

    fix: public ip

--- ec2.tf
+++ ec2.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "ec21" {
   network_interface {
     device_index         = 0
     network_interface_id = aws_network_interface.ec21.id
+    delete_on_termination = true
   }
   root_block_device {
     volume_type           = "gp3"
@@ -21,7 +22,7 @@ resource "aws_instance" "ec21" {
   user_data                   = base64encode(local.user_data_ec21)
   subnet_id                   = aws_subnet.public_subnet1.id
   security_groups             = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
+  associate_public_ip_address = aws_subnet.public_subnet1.map_public_ip_on_launch ? null : true
   private_ip                  = "192.168.50.10"
   tags = {
     Name = "MyServer"
